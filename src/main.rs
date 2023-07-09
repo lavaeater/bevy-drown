af3e275b2c733b3c0c15946cb38cdfb2bf9b3b3c
Tommie Nygren <tommie.nygren@gmail.com>
2023-07-09 19:53:06 +0200

    Hello world system added.

--- src/main.rs
+++ src/main.rs
@@ -8,40 +8,15 @@ fn main() {
             // by linear filtering.
             ImagePlugin::default_nearest(),
         ))
-        .add_startup_system(setup)
-        .add_system(sprite_movement)
+        .add_system(hello_world)
         .run();
 }
 
+pub fn hello_world() {
+    println!("Hello World!")
+}
+
 #[derive(Component)]
-enum Direction {
-    Left,
-    Right,
-}
-
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle::default());
-    commands.spawn((
-        SpriteBundle {
-            texture: asset_server.load("pixel/bevy_pixel_light.png"),
-            transform: Transform::from_xyz(100., 0., 0.),
-            ..default()
-        },
-        Direction::Right,
-    ));
-}
-
-fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, &mut Transform)>) {
-    for (mut logo, mut transform) in &mut sprite_position {
-        match *logo {
-            Direction::Right => transform.translation.x += 30. * time.delta_seconds(),
-            Direction::Left => transform.translation.x -= 30. * time.delta_seconds(),
-        }
-
-        if transform.translation.x > 200. {
-            *logo = Direction::Left;
-        } else if transform.translation.x < -200. {
-            *logo = Direction::Right;
-        }
-    }
+pub struct Person {
+    pub name: String 
 }
