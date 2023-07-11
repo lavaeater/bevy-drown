28d9ff8b9fb8f739fefc8d826a753766b054603e
Tommie Nygren <tommie.nygren@gmail.com>
2023-07-11 16:36:24 +0200

    Oooh, I managed to add the git repo for the reference, very cool stuff. Bleeding edge, eh!?

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,22 @@
 use bevy::prelude::*;
-use bevy_rapier2d::prelude::*;
+use bevy_xpbd_2d::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(
-            // This sets image filtering to nearest
-            // This is done to prevent textures with low resolution (e.g. pixel art) from being blurred
-            // by linear filtering.
-            ImagePlugin::default_nearest(),
-        ))
-        .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(32.0))
-        .add_plugins(RapierDebugRenderPlugin::default())
+        .add_plugins(
+            DefaultPlugins.set(
+                ImagePlugin::default_nearest(),
+            ),
+        )
+        .add_plugins(
+            PhysicsPlugins::default()
+                .build()
+        )
         .add_systems(Startup, spawn_camera)
         .add_systems(Startup, spawn_player)
-        .add_systems(Startup, setup_physics)
         .run();
 }
 
-fn setup_physics(mut commands: Commands) {
-    commands
-        .spawn(Collider::cuboid(500.0, 50.0))
-        .insert(TransformBundle::from(Transform::from_xyz(0.0, -100.0, 0.0)));
-
-    /* Create the bouncing ball. */
-    commands
-        .spawn(RigidBody::Dynamic)
-        .insert(Collider::ball(50.0))
-        .insert(Restitution::coefficient(0.95))
-        .insert(TransformBundle::from(Transform::from_xyz(0.0, 400.0, 0.0)));
-}
 
 #[derive(Component)]
 pub struct Player {}
@@ -40,7 +28,7 @@ pub fn spawn_player(
     commands.spawn(
         (
             SpriteBundle {
-                transform: Transform::from_xyz(0.0,0.0, 0.0),
+                transform: Transform::from_xyz(0.0, 0.0, 0.0),
                 texture: asset_server.load("sprites/head.png"),
                 ..default()
             },
