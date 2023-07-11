1dc2cfb73e7435d03bc81a29805ced62e39e1afc
Tommie Nygren <tommie.nygren@gmail.com>
2023-07-11 22:38:53 +0200

    Ball and head, but scale is off

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use bevy::prelude::*;
+use bevy::render::camera::ScalingMode;
 use bevy_xpbd_2d::prelude::*;
+
+const PIXELS_PER_METER: f32 = 32.0;
+const METERS_PER_PIXEL: f32 = 1.0 / PIXELS_PER_METER;
+const HEAD_SIZE: f32 = 8.0;
+
 
 fn main() {
     App::new()
@@ -39,20 +45,30 @@ pub fn spawn_player(
     commands.spawn(
         (
             SpriteBundle {
-                transform: Transform::from_xyz(0.0, 10.0, 0.0),
+                transform: Transform::from_xyz(0.0, 10.0, 0.0).with_scale(Vec3::new(METERS_PER_PIXEL,METERS_PER_PIXEL,METERS_PER_PIXEL)),
                 texture: asset_server.load("sprites/head.png"),
                 ..default()
             },
             Player {},
             RigidBody::Dynamic,
-            Collider::ball(0.35)
+            Collider::ball(HEAD_SIZE * METERS_PER_PIXEL)
         )
     );
 }
 
 pub fn spawn_camera(mut commands: Commands) {
+    let proj: OrthographicProjection = OrthographicProjection {
+        scale: 1.0,
+        near: 0.0,
+        far: 100.0,
+        viewport_origin: Vec2::new(0.5, 0.5),
+        scaling_mode: ScalingMode::WindowSize(PIXELS_PER_METER),
+        area: Rect::new(-1.0, -1.0, 1.0, 1.0),
+    };
+
     commands.spawn(Camera2dBundle {
         transform: Transform::from_xyz(0.0, 0.0, 1.0),
+        projection: proj,
         ..default()
     });
 }
